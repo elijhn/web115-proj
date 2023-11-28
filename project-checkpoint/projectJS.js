071895aa08501e5cbe6e3ef64bab5f81e6477ab7
elijhn <139831777+elijhn@users.noreply.github.com>
2023-11-28 14:22:18 -0500

    final update

--- project-checkpoint/projectJS.js
+++ project-checkpoint/projectJS.js
@@ -1,4 +1,5 @@
-
+// projectJS.js
+// Function to dynamically create meal input fields
 function createMealInputs() {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const meals = ['Breakfast', 'Snack', 'Lunch', 'Snack', 'Dinner'];
@@ -6,26 +7,27 @@ function createMealInputs() {
     let mealInputsHTML = '';
 
     days.forEach(day => {
-        mealInputsHTML += `<div class="day-meals"><h3>${day}</h3>`;
-        meals.forEach(meal => {
-            const inputId = `${day.toLowerCase()}${meal}`;
-            mealInputsHTML += `
-                <label for="${inputId}">${meal}:</label>
-                <input type="text" id="${inputId}">
-            `;
-        });
-        mealInputsHTML += `</div>`;
+    mealInputsHTML += `<div class="day-meals"><h3>${day}</h3>`;
+    meals.forEach(meal => {
+        const inputId = `${day.toLowerCase()}${meal}`;
+        mealInputsHTML += `<label for="${inputId}">${meal}:</label><input type="text" id="${inputId}">`;
+    });
+    mealInputsHTML += `</div>`;
     });
 
     document.getElementById('mealInputs').innerHTML = mealInputsHTML;
 }
+  createMealInputs();
 
-createMealInputs();
-
-// generate meal plan
+// Function to generate meal plan
 function generateMealPlan() {
-
+    // Validation
     const emailInput = document.getElementById('email').value;
+    if (!validateEmail(emailInput)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
     const name = document.getElementById('name').value;
     const goal = document.getElementById('goal').value;
 
@@ -41,6 +43,35 @@ function generateMealPlan() {
             mealPlan[day][meal] = document.getElementById(inputId).value;
         });
     });
+
+    // Create HTML content
+    let htmlContent = `<html>\n<head>\n<title>Your Meal Plan</title>\n</head>\n<body>\n`;
+    htmlContent += `<h1>Your Meal Plan</h1>`;
+    htmlContent += `<p>Name: ${name}</p>`;
+    htmlContent += `<p>Email: ${emailInput}</p>`;
+    htmlContent += `<p>Weekly Goal: ${goal}</p>`;
+    
+    htmlContent += `<h2>Meal Plan for the Week</h2>`;
+    for (const day in mealPlan) {
+        htmlContent += `<h3>${day}</h3>`;
+        htmlContent += `<ul>`;
+        for (const meal in mealPlan[day]) {
+            htmlContent += `<li><strong>${meal}:</strong> ${mealPlan[day][meal]}</li>`;
+        }
+        htmlContent += `</ul>`;
+    }
+
+    htmlContent += `</body>\n</html>`;
+
+    // Open a new window 
+    const flyWindow = window.open('about:blank', 'myPop', 'width=600,height=400,left=200,top=200');
+    flyWindow.document.write(htmlContent);
+}
+
+// validate email
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
 }
 
 // clear the meal planner
